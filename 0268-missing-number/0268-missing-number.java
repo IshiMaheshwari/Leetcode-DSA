6a209a4a4ab68bc76fa9d619a58416bce78c29d3
IshiMaheshwari <86151143+IshiMaheshwari@users.noreply.github.com>
2024-03-02 20:58:10 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (56.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int index,v;
-        for(v=0,index=0;index<nums.length;index++) v=((v^index)^nums[index]);
-        return (v^index);
+        int n=nums.length;
+        for(index=0,v=0;index<nums.length;index++)
+        {
+            v=(v^index)^nums[index];
+        }
+        return v^index;
     }
 }
